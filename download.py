0459518e5e82a46f1d8f1bc719e6490fbf876fbe
Gabriel Bevilacqua Barros <gabriel.bevilacqua.2002@gmail.com>
2024-09-06 16:33:39 -0300

    Pytubeflix

--- download.py
+++ download.py
@@ -1,24 +1,11 @@
 
-from pytube import YouTube
-
-
-print("Faça um download de um vídeo no youtube")
-
-url = input("Digite o link: ")
-
-video = YouTube(url)
-
-#Mudar tipo de video
-#streams = yt.streams.filter(file_extension='mp4') você pode mudar a extenção aqui
-
-#Download de audio
-#streams = video.streams.filter(only_audio=True)
-#stream = streams.order_by('abr').last()
-
-stream = video.streams.get_highest_resolution()
-#video.streams.get_lowest_resolution()  resolução menor
-#video.streams.get_highest_resolution() resolução maior
-#video.streams.get_by_resolution() primeira resolução que aparecer
-
-
-stream.download()
+from pytubefix import YouTube
+from pytubefix.cli import on_progress
+ 
+url = "https://youtu.be/vXbCWJNfq_0?si=CmKss2KIol1vZg5l"
+ 
+yt = YouTube(url, on_progress_callback = on_progress)
+print(yt.title)
+ 
+ys = yt.streams.get_highest_resolution()
+ys.download()
